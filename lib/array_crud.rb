40dd8803abca1cff4f212a1d85247ded5c6e8077
Dhara Patel <1991dharapatel@gmail.com>
2019-03-17 19:03:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   food = ["pizza", "puff", "khari", "oats"]
 end
 
-def add_element_to_end_of_array(food, eggs)
+def add_element_to_end_of_array(array, element)
   food.push("eggs")
 end
 
